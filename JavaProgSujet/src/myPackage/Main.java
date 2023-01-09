d6153ec9c9ced8e7cc33b1fc5c94353dd34884ba
usrlocal <usrlocal@GA319-FE13.ad.univ-angers.fr>
2023-01-09 14:31:48 +0100

    FirstCommitAlexandre

---
+++ JavaProgSujet/src/myPackage/Main.java
@@ -0,0 +1,12 @@
+package myPackage;
+
+public class Main {
+    public static void main(String[] args) 
+    {
+        DossierBancaire obj=new DossierBancaire();
+        GUI g = new GUI(obj);
+        //System.out.println("Solde: " + obj.get_solde());             
+    }
+    
+    //commentaire pour premier commit
+}
